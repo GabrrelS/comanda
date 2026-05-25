a57684d51cccb58cdfcf49bed2672ffa27958816
Gabriel <bielsousa5514@gmail.com>
2026-05-24 23:29:12 -0300

    ULTIMO COMMIT DOS TESTES

--- backend/app/routes/itens.py
+++ backend/app/routes/itens.py
@@ -1,4 +1,3 @@
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
@@ -10,7 +9,7 @@ print(">>> ITENS ROUTER IMPORTADO")
 
 router = APIRouter(
     prefix="/itens",
-    tags=["Itens"]
+    tags=["itens"]
 )
 
 
